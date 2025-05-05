33b4c94a7f9f1add3205fba6d278642314408278
Rafid Bin Bakhtiar <123709947+RafidBinBakhtiar@users.noreply.github.com>
2025-05-05 22:22:56 +0600

    Update-fix-final

---
+++ genwear/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['localhost'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+  },
+}
+
+module.exports = nextConfig
